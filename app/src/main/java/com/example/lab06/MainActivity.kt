98d5643d62dc41892ed1c12f302742e5845f5461
“Irsyad9898 <irsyad9813@gmail.com>
2024-05-25 14:15:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab06/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lab06
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.lab06.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.courses.setOnClickListener {
+            binding.infoTextView.text = "Android Application Development, Android Security, " +
+                    "Android UI UX and Monetize Android Application"
+        }
+
+
+        }
+    }
